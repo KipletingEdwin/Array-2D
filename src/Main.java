59973545174432c3e5b5cb71748a6b9c0cebc1d4
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-03-06 12:49:16 +0300

    add two methods of 2D array

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,55 @@
+
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //2D array = an array of arrays
+        String[][] cars = new String[3][3];
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        cars[0][0] = "Camaro";
+        cars[0][1] = "Corvette";
+        cars[0][2] = "Silverdo";
+
+        cars[1][0] = "Mustang";
+        cars[1][1] = "Ranger";
+        cars[1][2] = "F-150";
+
+        cars[2][0] = "Allion";
+        cars[2][1] = "Premio";
+        cars[2][2] = "Fielder";
+
+        for (int i =0; i<cars.length; i++){
+            System.out.println();
+            for(int j = 0; j<cars[i].length; j++){
+                System.out.print(cars[i][j]);
+
+            }
         }
+
+
+
+        //ANOTHER METHOD
+
+        String myCars[][] = { {"Camaro", "Corvette", "Silverdo"},
+                              {"Mustang", "Ranger", "F-150"},
+                              {"Allion", "Premio", "Fielder"}};
+
+
+
+
+        for (int i =0; i<myCars.length; i++){
+            System.out.println();
+            for(int j = 0; j<myCars[i].length; j++){
+                System.out.print(myCars[i][j]);
+
+            }
+        }
+
+
+
+
+
+
     }
 }
